6b1e354dc2cfd8cefd902e08b853914cc9dea73d
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2026-06-03 10:23:59 -0400

    Rename parameters in areBothOdd function

--- exercises/019-areBothOdd/app.js
+++ exercises/019-areBothOdd/app.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function areBothOdd(a, b) {
+function areBothOdd(num1, num2) {
     // your code here
     return false;
 }
