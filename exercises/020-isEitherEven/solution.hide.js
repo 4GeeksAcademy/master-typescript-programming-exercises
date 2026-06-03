0a053cc348bb8698a01df27089b88442721d1b22
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2026-06-03 10:25:21 -0400

    Update solution.hide.js

--- exercises/020-isEitherEven/solution.hide.js
+++ exercises/020-isEitherEven/solution.hide.js
@@ -1,5 +1,5 @@
 // Write your function here
-function isEitherEven(n1, n2) {
-  if (n1 % 2 == 0 || n2 % 2 == 0) return true;
+function isEitherEven(num1, num2) {
+  if (num1 % 2 == 0 || num2 % 2 == 0) return true;
   else return false;
 }
