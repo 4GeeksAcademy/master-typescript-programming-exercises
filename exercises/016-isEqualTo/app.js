590a83c8f40fc9df985e2d2a16703cde643d258b
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2026-06-03 10:20:22 -0400

    Rename parameters in isEqualTo function

--- exercises/016-isEqualTo/app.js
+++ exercises/016-isEqualTo/app.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function isEqualTo(a, b) {
+function isEqualTo(num1, num2) {
     // your code here
     return false;
 }
